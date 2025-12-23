c2ea1963d70a90683eb78ef7a21d861b21bf6db7
samuelpondugala <samueldavid.webdev@gmail.com>
2025-12-23 19:18:11 +0530

    Changed URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hms-backend-nkox.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
